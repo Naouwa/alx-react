c903a0f3148606b830e34d0a541d7fcea4f417c0
Naouwa <densali.nahla@gmail.com>
2024-09-03 00:39:13 +0100

    task_3

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -15,36 +15,46 @@ module.exports = {
     hints: false,
     maxEntrypointSize: 1000000,
   },
-  plugins: [ new CleanWebpackPlugin(), new HtmlWebpackPlugin() ],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Holberton Dashboard', // Add a title for your HTML file
+      template: path.resolve(__dirname, './template.html'), // Optionally specify a custom template
+    }),
+  ],
   optimization: {
     splitChunks: {
       chunks: 'all',
     },
   },
   devServer: {
-    contentBase: path.join(__dirname, './public'),
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
     compress: true,
     port: 8564,
+    open: true, // Automatically open the browser
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public'),
+    clean: true, // Clean the output directory before each build
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
-          "file-loader",
+          'file-loader',
           {
-            loader: "image-webpack-loader",
+            loader: 'image-webpack-loader',
             options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
+              bypassOnDebug: true, // Disable optimization during debugging
+              disable: true, // Disable image optimization
             },
           },
         ],
