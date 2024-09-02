8d511afe6cfc4264dc1491baef26a0438953fb01
Naouwa <densali.nahla@gmail.com>
2024-09-02 23:49:04 +0100

    task_2

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        main: path.resolve(__dirname, './js/dashboard_main.js'),
+    
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+          type: 'asset/resource',
+          use: [
+            'file-loader',
+					  {
+						  loader: 'image-webpack-loader',
+						  options: {
+							  bypassOnDebug: true,
+							  disable: true,
+						  },
+					  },
+				  ],
+        },
+      ]
+    }
+  }
+};
