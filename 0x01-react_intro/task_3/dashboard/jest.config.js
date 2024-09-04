9dad74b3eeb7aecae59064227d30e92a07bbf261
Naouwa <densali.nahla@gmail.com>
2024-09-04 19:29:42 +0100

    task7

--- 0x01-react_intro/task_3/dashboard/jest.config.js
+++ 0x01-react_intro/task_3/dashboard/jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   transform: {
     "^.+\\.(js|jsx)$": "babel-jest"
   },
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
+  },
+  setupFiles: ["<rootDir>/src/setupTests.js"]
 };
