3cd6206edc683a810f257d53320a55f5a245c5a3
Naouwa <densali.nahla@gmail.com>
2024-09-04 21:50:51 +0100

    task11

---
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,5 @@
+// Function to get the current year
+export const getFullYear = () =>  new Date().getFullYear();
+
+// Function to get footer text based on isIndex
+export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton Schook main dashboard");
